7c1f6185d8371d57d2ef3d8be39bf0672414dff8
riittagerhards <rgerhards@outlook.com>
2021-10-15 12:20:35 +0200

    Add input elements

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import { createTitleComponent } from "./lib/createTitleComponent.js";
 import { createInstructionComponent } from "./lib/createInstructionComponent.js";
+import { createInputComponent } from "./lib/createInputComponent.js";
 
 function createApp() {
   const appElement = document.querySelector("#app");
   const titleComponent = createTitleComponent();
   const instructionComponent = createInstructionComponent();
+  const inputComponent = createInputComponent();
 
   appElement.append(titleComponent);
   appElement.append(instructionComponent);
+  appElement.append(inputComponent);
 }
 
 createApp();
