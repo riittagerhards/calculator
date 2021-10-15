c5df8ef879238e52c3ef5984affc490a29cdfc72
riittagerhards <rgerhards@outlook.com>
2021-10-15 11:43:56 +0200

    Add title and instruction components

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import { createEnterBirthdayPage } from "./lib/enterBirthdayPage.js";
+import { createTitleComponent } from "./lib/createTitleComponent.js";
+import { createInstructionComponent } from "./lib/createInstructionComponent.js";
+
+function createApp() {
+  const appElement = document.querySelector("#app");
+  const titleComponent = createTitleComponent();
+  const instructionComponent = createInstructionComponent();
+
+  appElement.append(titleComponent);
+  appElement.append(instructionComponent);
+}
+
+createApp();
