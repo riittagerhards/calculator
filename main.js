41ac337cfb5a63d92ade61d22bd21167c9d91e13
Riitta Gerhards <91462609+riittagerhards@users.noreply.github.com>
2021-10-15 22:12:16 +0200

    Revert "Add calculation function"

--- main.js
+++ main.js
@@ -1,36 +1,16 @@
 import { createTitleComponent } from "./lib/createTitleComponent.js";
 import { createInstructionComponent } from "./lib/createInstructionComponent.js";
 import { createInputComponent } from "./lib/createInputComponent.js";
-import { createButtonComponent } from "./lib/createButtonComponent.js";
-import { createElement } from "./lib/elements.js";
 
 function createApp() {
   const appElement = document.querySelector("#app");
-
   const titleComponent = createTitleComponent();
   const instructionComponent = createInstructionComponent();
+  const inputComponent = createInputComponent();
 
   appElement.append(titleComponent);
   appElement.append(instructionComponent);
-
-  const firstNumber = createInputComponent("");
-  const secondNumber = createInputComponent("");
-  appElement.append(firstNumber, secondNumber);
-
-  const wrapperElement = createElement("div", {
-    className: "wrapper",
-  });
-
-  appElement.append(wrapperElement);
-
-  const calcSum = createButtonComponent("+", function () {
-    alert(Number(firstNumber.value) + Number(secondNumber.value));
-  });
-  const calcDiff = createButtonComponent("-", function () {
-    alert(Number(firstNumber.value) - Number(secondNumber.value));
-  });
-
-  wrapperElement.append(calcSum, calcDiff);
+  appElement.append(inputComponent);
 }
 
 createApp();
